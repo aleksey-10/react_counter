85333dad1a39b30869d7cb8d03505fc255478c8e
aleksey-10 <alexcare1337@gmail.com>
2020-06-16 17:57:28 +0300

    Updates

---
+++ precache-manifest.7e682eae1a32737b538b8f1cee0c1536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580ae86ae76f9c398bcf687b75ab8578",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28c44bde5cc162ae9fa7",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "a7a8d4fcc6be633408b2",
+    "url": "./static/js/2.b27bf6c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b27bf6c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c44bde5cc162ae9fa7",
+    "url": "./static/js/main.d7f10178.chunk.js"
+  },
+  {
+    "revision": "0fce0ea25cea0681ed78",
+    "url": "./static/js/runtime-main.2af7c007.js"
+  }
+]);
